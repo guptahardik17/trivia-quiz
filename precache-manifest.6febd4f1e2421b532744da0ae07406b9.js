ea2b9fb9b990402aa132c12155ab7a162103a8dd
guptahardik17 <guptahardik17@gmail.com>
2019-03-22 10:14:11 +0530

    Updates

---
+++ precache-manifest.6febd4f1e2421b532744da0ae07406b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "54fc6e52189d7ece81b6",
+    "url": "/trivia-quiz/static/css/main.a2095b0d.chunk.css"
+  },
+  {
+    "revision": "54fc6e52189d7ece81b6",
+    "url": "/trivia-quiz/static/js/main.9801e744.chunk.js"
+  },
+  {
+    "revision": "547037cd7116d88c8857",
+    "url": "/trivia-quiz/static/js/runtime~main.75badddd.js"
+  },
+  {
+    "revision": "f22f1f1c0accc527360a",
+    "url": "/trivia-quiz/static/js/2.e04b6f8b.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/trivia-quiz/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "10ea854b19062a5eeb7b32f0c0789ef2",
+    "url": "/trivia-quiz/index.html"
+  }
+];
